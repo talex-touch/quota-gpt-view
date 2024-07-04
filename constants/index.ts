03b1a4113b23ae80dc223610dcb463a93b7b210f
TalexDreamSoul <2418876761@qq.com>
2024-07-04 22:00:08 +0800

    update ends url

--- constants/index.ts
+++ constants/index.ts
@@ -1,6 +1,6 @@
 export const appName = 'ThisAI - Quota'
-export const appDescription = 'Vitesse for Nuxt 3'
+export const appDescription = 'THISAI'
 
 export const ENDS_URL = {
-  dev: 'https://quota_api.tagzxia.com:7001',
+  dev: 'https://quota.api.tagzxia.com:7001',
 }
