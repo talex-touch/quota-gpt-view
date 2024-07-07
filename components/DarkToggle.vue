dac021053f625c5a3a67131400362dcc8ef0f084
TalexDreamSoul <2418876761@qq.com>
2024-07-07 08:18:09 +0800

    feat: color changed animation

--- components/DarkToggle.vue
+++ components/DarkToggle.vue
@@ -2,24 +2,66 @@
 const color = useColorMode()
 
 useHead({
-  meta: [{
-    id: 'theme-color',
-    name: 'theme-color',
-    content: () => color.value === 'dark' ? '#222222' : '#ffffff',
-  }],
+  meta: [
+    {
+      id: 'theme-color',
+      name: 'theme-color',
+      content: () => (color.value === 'dark' ? '#222222' : '#ffffff'),
+    },
+  ],
 })
 
-function toggleDark() {
-  color.preference = color.value === 'dark' ? 'light' : 'dark'
+function viewTransition(e: { clientX: number, clientY: number }) {
+  if (!document.startViewTransition)
+    return
+
+  const transition = document.startViewTransition(() => {
+    color.preference = color.value === 'dark' ? 'light' : 'dark'
+
+    document.body.classList.toggle('dark', color.value === 'dark')
+  })
+
+  transition.ready.then(() => {
+    const { clientX, clientY } = e
+
+    const radius = Math.hypot(
+      Math.max(clientX, innerWidth - clientX),
+      Math.max(clientY, innerHeight - clientY),
+    )
+    const clipPath = [
+      `circle(0% at ${clientX}px ${clientY}px)`,
+      `circle(${radius}px at ${clientX}px ${clientY}px)`,
+    ]
+    const isDark = document.documentElement.classList.contains('dark')
+
+    document.documentElement.animate(
+      {
+        clipPath: isDark ? clipPath.reverse() : clipPath,
+      },
+      {
+        duration: 500,
+        pseudoElement: isDark
+          ? '::view-transition-old(root)'
+          : '::view-transition-new(root)',
+      },
+    )
+  })
 }
-
-watch(() => color.value, (val) => {
-  document.body.classList.toggle('dark', val === 'dark')
-})
 </script>
 
 <template>
-  <button class="!outline-none" @click="toggleDark">
+  <button class="!outline-none" @click="viewTransition">
     <div class="i-carbon-sun dark:i-carbon-moon" />
   </button>
 </template>
+
+<style>
+::view-transition-new(root),
+::view-transition-old(root) {
+  animation: none;
+}
+
+.dark::view-transition-old(root) {
+  z-index: 100;
+}
+</style>
