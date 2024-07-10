84da02545bdf89c7fba899f79be7721599f88d7f
TalexDreamSoul <2418876761@qq.com>
2024-07-11 00:13:21 +0800

    feat: offline mention mask

--- layouts/default.vue
+++ layouts/default.vue
@@ -5,12 +5,14 @@ const online = useOnline()
 <template>
   <Suspense>
     <ClientOnly>
-      <main v-if="online" class="DefaultTemplate-Container">
+      <main class="DefaultTemplate-Container">
         <slot />
+
+        <div class="Offline" :class="{ display: online }">
+          <div v-for="i in 4" :key="i" :class="`item-${i}`" class="item" />
+          <!-- 你已经离线了！ -->
+        </div>
       </main>
-      <div v-else text-gray:80>
-        你已经离线了！
-      </div>
     </ClientOnly>
     <template #fallback>
       <div italic op50>
@@ -21,6 +23,75 @@ const online = useOnline()
 </template>
 
 <style lang="scss">
+.Offline {
+  &.display {
+    .item-1 {
+      right: -100%;
+      bottom: -100%;
+    }
+
+    .item-2 {
+      top: -100%;
+      right: -100%;
+    }
+
+    .item-3 {
+      left: -100%;
+      bottom: -100%;
+    }
+
+    .item-4 {
+      top: -100%;
+      left: -100%;
+    }
+
+    pointer-events: none;
+    backdrop-filter: blur(0);
+  }
+  .item-1 {
+    right: 0;
+    bottom: 0;
+  }
+
+  .item-2 {
+    top: 0;
+    right: 0;
+  }
+
+  .item-3 {
+    left: 0;
+    bottom: 0;
+  }
+
+  .item-4 {
+    top: 0;
+    left: 0;
+  }
+
+  .item {
+    position: absolute;
+
+    width: 50%;
+    height: 50%;
+
+    opacity: 0.75;
+    background-color: var(--el-bg-color-overlay);
+    transition: 0.5s cubic-bezier(0.785, 0.135, 0.15, 0.86);
+  }
+  z-index: 1000;
+  position: absolute;
+
+  top: 0;
+  left: 0;
+
+  width: 100%;
+  height: 100%;
+
+  // color: var(--el-color-danger);
+  backdrop-filter: blur(18px);
+  transition: 0.75s cubic-bezier(0.785, 0.135, 0.15, 0.86);
+}
+
 .DefaultTemplate-Container {
   position: absolute;
   display: flex;
