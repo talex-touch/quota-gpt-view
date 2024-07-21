4942a8ce7348b49de767349728881d4e84043a87
TalexDreamSoul <2418876761@qq.com>
2024-07-21 10:18:45 +0800

    feat: history chat relatives

--- pages/index.vue
+++ pages/index.vue
@@ -150,7 +150,7 @@ provide('pageOptions', pageOptions)
       <ShareSection :length="chatManager.messages.value.messages.length" :show="pageOptions.share.enable" :selected="pageOptions.share.selected" />
 
       <div class="copyright">
-        ThisAI. 可能会犯错，生成的内容仅供参考。v24.07.20
+        ThisAI. 可能会犯错，生成的内容仅供参考。v24.07.21
         <span class="business">四川科塔锐行科技有限公司</span>
       </div>
     </div>
