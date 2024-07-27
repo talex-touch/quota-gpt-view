053c19128e5596eaba126ab3be0568818a175b88
TalexDreamSoul <2418876761@qq.com>
2024-07-27 21:25:38 +0800

    参数配置和流量监控初始化

---
+++ pages/cms/aigc/log.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+definePageMeta({
+  name: '流量监控',
+  layout: 'cms',
+  pageTransition: {
+    name: 'rotate',
+  },
+})
+</script>
+
+<template>
+  <div class="Cms">
+    1
+  </div>
+</template>
+
+<style lang="scss">
+.Cms {
+  background: red;
+}
+</style>
