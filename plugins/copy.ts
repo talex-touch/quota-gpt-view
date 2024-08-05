b8944cd1348592cafeed1e5c73db91017a461c2b
TalexDreamSoul <2418876761@qq.com>
2024-08-05 16:23:41 +0800

    refactor(copy): add type annotation to copyValue function
    Added a type annotation to the copyValue function's parameter to improve code
    readability and prevent potential type errors in the future. Additionally,
    unnecessary comments and whitespace were removed for cleanliness.

--- plugins/copy.ts
+++ plugins/copy.ts
@@ -1,6 +1,6 @@
 import { createApp } from 'vue'
 
-function copyValue(bindings) {
+function copyValue(bindings: any) {
   return () => {
     if (bindings.value) {
       // copy to clipboard
@@ -10,7 +10,11 @@ function copyValue(bindings) {
     }
   }
 }
-
+/**
+ * 定义一个Nuxt插件，用于在Vue组件中添加点击复制功能
+ * @param {object} nuxtApp - Nuxt应用实例
+ * @returns {object} 返回一个Nuxt插件配置对象
+ */
 export default defineNuxtPlugin((nuxtApp) => {
   nuxtApp.vueApp.directive('copy', {
     mounted(el: HTMLElement, bindings) {
