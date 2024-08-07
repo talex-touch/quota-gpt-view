29762476bc47112063776b834867a2a67d669420
TalexDreamSoul <2418876761@qq.com>
2024-08-08 02:29:00 +0800

    chore: update version in index page footer
    Update the version date in the footer from v24.08.07 to v24.08.08 to reflect
    the latest changes and improvements made to the page.

--- pages/index.vue
+++ pages/index.vue
@@ -162,7 +162,7 @@ provide('pageOptions', pageOptions)
       />
 
       <div class="copyright">
-        ThisAI. 可能会犯错，生成的内容仅供参考。v24.08.07
+        ThisAI. 可能会犯错，生成的内容仅供参考。v24.08.08
         <span class="business">四川科塔锐行科技有限公司</span>
       </div>
 
