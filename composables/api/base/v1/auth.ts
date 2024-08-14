a67416214ffa54d475acd11c18d47dba30fb70a2
TalexDreamSoul <2418876761@qq.com>
2024-08-14 14:45:00 +0800

    feat(auth): add token refresh mechanism for API requests
    Implement token refresh functionality to handle expired tokens by automatically
    renewing them in the background. This avoids interruptions by transparently
    managing token lifecycle for improved user experience.
    
    The changes include updates to the axios composables for handling pending
    requests, adding a token refresh method, and modifying the error handling to
    trigger the token renewal process when necessary. Additionally, a new renewToken
    method was introduced in the auth API to facilitate the token renewal process.

--- composables/api/base/v1/auth.ts
+++ composables/api/base/v1/auth.ts
@@ -1,7 +1,11 @@
 import { endHttp } from '../../axios'
+import type { ILoginToken } from './auth.type'
 
 export default {
   serverStatus() {
     return endHttp.get('auth/status')
+  },
+  renewToken(): ILoginToken {
+    return endHttp.get('auth/renew_token')
   },
 }
