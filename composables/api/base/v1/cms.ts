179846ae8564ea447ffa148831e1be3d7bd889fa
点正 <1960825664@qq.com>
2024-08-14 18:13:56 +0800

    用户菜单 api

--- composables/api/base/v1/cms.ts
+++ composables/api/base/v1/cms.ts
@@ -1,8 +1,28 @@
 import { endHttp } from '../../axios'
 import type { IDataResponse, IPageResponse, IStandardPageModel, IStandardResponse } from '../index.type'
-import type { IDictItemModel, IDictItemModelQuery, IRoleModel, IRoleModelQuery } from './cms.type'
+import type { IDictItemModel, IDictItemModelQuery, IRoleModel, IRoleModelQuery, IUserModel, IUserModelQuery } from './cms.type'
 
 export default {
+
+  users: {
+    list(query: IUserModelQuery) {
+      return endHttp.get('system/roles', query) as Promise<IPageResponse<IUserModel>>
+    },
+    create(Body : IUserModel) {
+      return endHttp.post('system/roles', Body )
+    },
+    get(id: number | string) {
+      return endHttp.get(`system/roles/${id}`) as Promise<IDataResponse<IUserModel>>
+    },
+    update(id: number | string, Body : IUserModel) {
+      return endHttp.put(`system/roles/${id}`, Body ) as Promise<IDataResponse<IUserModel>>
+    },
+    delete(id: number | string) {
+      return endHttp.del(`system/roles/${id}`) as Promise<IStandardResponse>
+    },
+  },
+
+
   role: {
     list(query: IRoleModelQuery) {
       return endHttp.get('system/roles', query) as Promise<IPageResponse<IRoleModel>>
@@ -37,4 +57,22 @@ export default {
       return endHttp.del(`system/dict-item/${id}`) as Promise<IStandardResponse>
     },
   },
+
+  // paramConfig: {
+  //   list(query: IDictItemModelQuery) {
+  //     return endHttp.get('system/dict-item', query) as Promise<IPageResponse<IDictItemModel>>
+  //   },
+  //   create(query: IDictItemModel) {
+  //     return endHttp.post('system/dict-item', query)
+  //   },
+  //   get(id: number | string) {
+  //     return endHttp.get(`system/dict-item/${id}`) as Promise<IDataResponse<IDictItemModel>>
+  //   },
+  //   update(id: number | string, query: IDictItemModel) {
+  //     return endHttp.post(`system/dict-item/${id}`, query) as Promise<IDataResponse<IDictItemModel>>
+  //   },
+  //   delete(id: number | string) {
+  //     return endHttp.del(`system/dict-item/${id}`) as Promise<IStandardResponse>
+  //   },
+  // },
 }
--- composables/api/base/v1/cms.type.d.ts
+++ composables/api/base/v1/cms.type.d.ts
@@ -1,3 +1,69 @@
+export interface IUserModel extends Record<string, any> {
+
+     /**
+     * 头像
+     */
+     avatar?: string;
+     /**
+      * 归属大区
+      */
+     deptId?: number;
+     /**
+      * 邮箱
+      */
+     email?: string;
+   
+     /**
+      * 呢称
+      */
+     nickname?: string;
+  
+ 
+     /**
+      * 登录密码
+      */
+     password?: string;
+     /**
+      * 手机号
+      */
+     phone?: string;
+     /**
+      * QQ
+      */
+     qq?: string;
+     /**
+      * 备注
+      */
+     remark?: string;
+     /**
+      * 归属角色
+      */
+     roleIds?: string[];
+     /**
+      * 状态
+      */
+     status?: number;
+     /**
+      * 登录账号
+      */
+     username?: string;
+
+
+
+
+  id?: number
+  updatedAt?: string
+  createdAt?: string
+}
+export interface IUserModelQuery extends IUserModel {
+  page: number
+  pageSize: number
+}
+
+
+
+
+
 export interface IRoleModel extends Record<string, any> {
   /**
    * 关联菜单、权限编号
@@ -30,6 +96,77 @@ export interface IRoleModelQuery extends IRoleModel {
   page: number
   pageSize: number
 }
+
+
+export interface IMenuModel extends Record<string, any> {
+      /**
+     * 设置当前路由高亮的菜单项，一般用于详情页
+     */
+      activeMenu?: string;
+      /**
+       * 菜单路由路径或外链
+       */
+      component?: string;
+      /**
+       * 外链打开方式
+       */
+      extOpenMode?: number;
+      /**
+       * 菜单图标
+       */
+      icon?: string;
+      /**
+       * 是否外链
+       */
+      isExt?: boolean;
+      /**
+       * 是否开启页面缓存
+       */
+      keepAlive?: number;
+      /**
+       * 菜单或权限名称
+       */
+      name?: string;
+      /**
+       * 排序
+       */
+      orderNo?: number;
+      /**
+       * 父级菜单
+       */
+      parentId?: number;
+      /**
+       * 前端路由地址
+       */
+      path?: string;
+      /**
+       * 对应权限
+       */
+      permission?: string;
+      /**
+       * 菜单是否显示
+       */
+      show?: number;
+      /**
+       * 状态
+       */
+      status?: number;
+      /**
+       * 菜单类型
+       */
+      type?: number;
+
+}
+//这里这个菜单疑惑有没有page,pagesize
+export interface IMenuModelQuery extends IMenuModel {
+  page: number
+  pageSize: number
+}
+
+
+
+
+
 
 export interface IDictItemModel extends Record<string, any> {
   /**
