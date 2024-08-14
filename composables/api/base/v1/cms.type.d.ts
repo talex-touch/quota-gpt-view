b552eb9a7f70761235e7b145bad50809ec64b052
Mikey <3425653390@qq.com>
2024-08-14 18:57:36 +0800

    fix

--- composables/api/base/v1/cms.type.d.ts
+++ composables/api/base/v1/cms.type.d.ts
@@ -1,55 +1,51 @@
 export interface IUserModel extends Record<string, any> {
 
-     /**
-     * 头像
-     */
-     avatar?: string;
-     /**
-      * 归属大区
-      */
-     deptId?: number;
-     /**
-      * 邮箱
-      */
-     email?: string;
-   
-     /**
-      * 呢称
-      */
-     nickname?: string;
-  
- 
-     /**
-      * 登录密码
-      */
-     password?: string;
-     /**
-      * 手机号
-      */
-     phone?: string;
-     /**
-      * QQ
-      */
-     qq?: string;
-     /**
-      * 备注
-      */
-     remark?: string;
-     /**
-      * 归属角色
-      */
-     roleIds?: string[];
-     /**
-      * 状态
-      */
-     status?: number;
-     /**
-      * 登录账号
-      */
-     username?: string;
+  /**
+   * 头像
+   */
+  avatar?: string
+  /**
+   * 归属大区
+   */
+  deptId?: number
+  /**
+   * 邮箱
+   */
+  email?: string
 
+  /**
+   * 呢称
+   */
+  nickname?: string
 
-
+  /**
+   * 登录密码
+   */
+  password?: string
+  /**
+   * 手机号
+   */
+  phone?: string
+  /**
+   * QQ
+   */
+  qq?: string
+  /**
+   * 备注
+   */
+  remark?: string
+  /**
+   * 归属角色
+   */
+  roleIds?: string[]
+  /**
+   * 状态
+   */
+  status?: number
+  /**
+   * 登录账号
+   */
+  username?: string
 
   id?: number
   updatedAt?: string
@@ -59,10 +55,6 @@ export interface IUserModelQuery extends IUserModel {
   page: number
   pageSize: number
 }
-
-
-
-
 
 export interface IRoleModel extends Record<string, any> {
   /**
@@ -97,83 +89,73 @@ export interface IRoleModelQuery extends IRoleModel {
   pageSize: number
 }
 
-<<<<<<< HEAD
-export interface IParamConfigModel extends Record<string, any> {
-
-=======
-
 export interface IMenuModel extends Record<string, any> {
-      /**
-     * 设置当前路由高亮的菜单项，一般用于详情页
-     */
-      activeMenu?: string;
-      /**
-       * 菜单路由路径或外链
-       */
-      component?: string;
-      /**
-       * 外链打开方式
-       */
-      extOpenMode?: number;
-      /**
-       * 菜单图标
-       */
-      icon?: string;
-      /**
-       * 是否外链
-       */
-      isExt?: boolean;
-      /**
-       * 是否开启页面缓存
-       */
-      keepAlive?: number;
-      /**
-       * 菜单或权限名称
-       */
-      name?: string;
-      /**
-       * 排序
-       */
-      orderNo?: number;
-      /**
-       * 父级菜单
-       */
-      parentId?: number;
-      /**
-       * 前端路由地址
-       */
-      path?: string;
-      /**
-       * 对应权限
-       */
-      permission?: string;
-      /**
-       * 菜单是否显示
-       */
-      show?: number;
-      /**
-       * 状态
-       */
-      status?: number;
-      /**
-       * 菜单类型
-       */
-      type?: number;
+  /**
+   * 设置当前路由高亮的菜单项，一般用于详情页
+   */
+  activeMenu?: string
+  /**
+   * 菜单路由路径或外链
+   */
+  component?: string
+  /**
+   * 外链打开方式
+   */
+  extOpenMode?: number
+  /**
+   * 菜单图标
+   */
+  icon?: string
+  /**
+   * 是否外链
+   */
+  isExt?: boolean
+  /**
+   * 是否开启页面缓存
+   */
+  keepAlive?: number
+  /**
+   * 菜单或权限名称
+   */
+  name?: string
+  /**
+   * 排序
+   */
+  orderNo?: number
+  /**
+   * 父级菜单
+   */
+  parentId?: number
+  /**
+   * 前端路由地址
+   */
+  path?: string
+  /**
+   * 对应权限
+   */
+  permission?: string
+  /**
+   * 菜单是否显示
+   */
+  show?: number
+  /**
+   * 状态
+   */
+  status?: number
+  /**
+   * 菜单类型
+   */
+  type?: number
 
 }
-//这里这个菜单疑惑有没有page,pagesize
+// 这里这个菜单疑惑有没有page,pagesize
 export interface IMenuModelQuery extends IMenuModel {
   page: number
   pageSize: number
 }
 
+export interface IParamConfigModel extends Record<string, any> {
 
-
-
-
-
-export interface IDictItemModel extends Record<string, any> {
->>>>>>> 2a0133309d0ca3d21566298f5fecc81ab9b4d092
   /**
    * 参数名称
    */
@@ -317,7 +299,6 @@ export interface ITasksModelQuery extends ITasksModel {
 
 }
 
-
 // 系统监控
 /**
  * ServeStatInfo
@@ -326,137 +307,137 @@ export interface ServeStatInfo {
   /**
    * CPU信息
    */
-  cpu: Cpu;
+  cpu: Cpu
   /**
    * 磁盘信息
    */
-  disk: Disk;
+  disk: Disk
   /**
    * 内存信息
    */
-  memory: Memory;
+  memory: Memory
   /**
    * 运行环境
    */
-  runtime: Runtime;
-  [property: string]: any;
+  runtime: Runtime
+  [property: string]: any
 }
 
 /**
-* CPU信息
-*
-* Cpu
-*/
+ * CPU信息
+ *
+ * Cpu
+ */
 export interface Cpu {
   /**
    * 品牌
    */
-  brand: string;
+  brand: string
   /**
    * cpu资源消耗
    */
-  coresLoad: CoreLoad[];
+  coresLoad: CoreLoad[]
   /**
    * 制造商
    */
-  manufacturer: string;
+  manufacturer: string
   /**
    * 型号
    */
-  model: string;
+  model: string
   /**
    * 物理核心数
    */
-  physicalCores: number;
+  physicalCores: number
   /**
    * CPU资源消耗 原始滴答
    */
-  rawCurrentLoad: number;
+  rawCurrentLoad: number
   /**
    * 空闲CPU资源 原始滴答
    */
-  rawCurrentLoadIdle: number;
+  rawCurrentLoadIdle: number
   /**
    * 速度 in GHz
    */
-  speed: number;
-  [property: string]: any;
+  speed: number
+  [property: string]: any
 }
 
 /**
-* CoreLoad
-*/
+ * CoreLoad
+ */
 export interface CoreLoad {
   /**
    * 当前CPU资源消耗
    */
-  rawLoad: number;
+  rawLoad: number
   /**
    * 当前空闲CPU资源
    */
-  rawLoadIdle: number;
-  [property: string]: any;
+  rawLoadIdle: number
+  [property: string]: any
 }
 
 /**
-* 磁盘信息
-*
-* Disk
-*/
+ * 磁盘信息
+ *
+ * Disk
+ */
 export interface Disk {
   /**
    * 可用磁盘空间 (bytes)
    */
-  available: number;
+  available: number
   /**
    * 磁盘空间大小 (bytes)
    */
-  size: number;
+  size: number
   /**
    * 已使用磁盘空间 (bytes)
    */
-  used: number;
-  [property: string]: any;
+  used: number
+  [property: string]: any
 }
 
 /**
-* 内存信息
-*
-* Memory
-*/
+ * 内存信息
+ *
+ * Memory
+ */
 export interface Memory {
   /**
    * 可用内存
    */
-  available: number;
+  available: number
   /**
    * total memory in bytes
    */
-  total: number;
-  [property: string]: any;
+  total: number
+  [property: string]: any
 }
 
 /**
-* 运行环境
-*
-* Runtime
-*/
+ * 运行环境
+ *
+ * Runtime
+ */
 export interface Runtime {
   /**
    * 服务器架构
    */
-  arch: string;
+  arch: string
   /**
    * Node版本
    */
-  nodeVersion: string;
+  nodeVersion: string
   /**
    * Npm版本
    */
-  npmVersion: string;
+  npmVersion: string
   /**
    * 系统
    */
-  os: string;
-  [property: string]: any;
+  os: string
+  [property: string]: any
 }
