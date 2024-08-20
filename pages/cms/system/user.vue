903e458de9f7c4ce7f0e5db5ecbc427a6c409aec
TalexDreamSoul <2418876761@qq.com>
2024-08-20 20:48:36 +0800

    ``` feat(user-table): adjust table height and pagination options
    Change the user table height to 90% to accommodate more users per page. Update
    pagination size options to include 15, 30, 50, and 100 items per page for
    better navigation and readability.
    ```

--- pages/cms/system/user.vue
+++ pages/cms/system/user.vue
@@ -316,7 +316,7 @@ function filterNode(value: string, data: any) {
       </el-form>
 
       <ClientOnly>
-        <el-table v-if="users?.items" :data="users.items" style="width: 100%">
+        <el-table v-if="users?.items" :data="users.items" height="90%">
           <el-table-column type="index" label="序号" width="60" />
           <el-table-column prop="date" label="头像" width="80">
             <template #default="scope">
@@ -381,7 +381,7 @@ function filterNode(value: string, data: any) {
 
         <el-pagination
           v-if="users?.meta" v-model:current-page="users.meta.currentPage"
-          v-model:page-size="users.meta.itemsPerPage" float-right my-4 :page-sizes="[10, 30, 50, 100]"
+          v-model:page-size="users.meta.itemsPerPage" float-right my-4 :page-sizes="[15, 30, 50, 100]"
           layout="total, sizes, prev, pager, next, jumper" :total="users.meta.totalItems" @change="fetchData"
         />
       </ClientOnly>
