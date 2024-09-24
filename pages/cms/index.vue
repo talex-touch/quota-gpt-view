e28dc549d97de01c5619383b2d4fb065a28a959a
Mikey <3425653390@qq.com>
2024-09-24 21:50:09 +0800

    更改index页面布局

--- pages/cms/index.vue
+++ pages/cms/index.vue
@@ -18,23 +18,24 @@ definePageMeta({
         </div>
       </div>
     </div>
-    <div class="next">
-      <div>a</div>
-      <div>b</div>
+    <div class="CmsAnalyze">
+      <div />
+      <div />
     </div>
-    <div class="rix">
-      <div class="box1">
-        1
+    <div class="CmsDisplay">
+      <div class="CmsDisplay-Greater" />
+
+      <div h-full flex flex-1 gap-4>
+        <div class="CmsDisplay-Lesser1" />
+        <div class="CmsDisplay-Lesser2" />
       </div>
-      <div>2</div>
-      <div>3</div>
     </div>
-    <div class="button">
-      <div class="box2">
-        1
+    <div class="CmsDisplay2">
+      <div class="CmsDisplay2-Greater" />
+      <div h-full flex flex-1 gap-4>
+        <div class="CmsDisplay-Lesser1" />
+        <div class="CmsDisplay-Lesser2" />
       </div>
-      <div>2</div>
-      <div>3</div>
     </div>
   </div>
 </template>
@@ -47,7 +48,7 @@ definePageMeta({
   width: 100%;
   height: 100%;
 
-  gap: 0.5rem;
+  gap: 1rem;
   flex-direction: column;
 
   justify-content: space-between;
@@ -73,6 +74,55 @@ definePageMeta({
       border-radius: 50%;
       border: 1px solid var(--el-text-color-primary);
     }
+    padding: 1rem 1rem;
+
+    flex: 1;
+
+    border-radius: 18px;
+    background-color: var(--el-fill-color-light);
+  }
+}
+
+.CmsAnalyze {
+  display: flex;
+
+  gap: 1rem;
+  width: 100%;
+
+  // padding: 1rem 0.5rem;
+
+  flex: 1;
+}
+
+.CmsAnalyze > div {
+  flex: 1;
+
+  border-radius: 18px;
+  background-color: var(--el-fill-color-light);
+}
+
+.CmsDisplay {
+  display: flex;
+  gap: 1rem;
+  width: 100%;
+  flex: 1;
+  &-Greater {
+    flex: 1;
+
+    // width: 50%;
+
+    border-radius: 18px;
+    background-color: var(--el-fill-color-light);
+  }
+  &-Lesser1 {
+    padding: 1rem 0.5rem;
+
+    flex: 1;
+
+    border-radius: 18px;
+    background-color: var(--el-fill-color-light);
+  }
+  &-Lesser2 {
     padding: 1rem 0.5rem;
 
     flex: 1;
@@ -82,78 +132,53 @@ definePageMeta({
   }
 }
 
-.next {
-  display: grid;
-  grid-template-columns: repeat(2, 1fr);
-  grid-template-rows: 175px; /* 增加高度 */
-  grid-gap: 2rem;
-  width: 100%;
-  height: auto;
-}
+// .CmsDisplay > div {
+//   padding: 1rem 0.5rem;
 
-.next > div {
-  background-color: #fff;
-  border: 1px solid #000;
-  border-radius: 4px;
-  padding: 15px; /* 增加内边距 */
-  font-size: 1.2em; /* 增加字体大小 */
-}
+//   flex: 1;
 
-.rix {
-  display: grid;
-  grid-template-columns: repeat(4, 1fr);
-  grid-template-rows: 175px; /* 增加高度 */
-  grid-gap: 2rem;
-  width: 100%;
-  height: auto;
-}
+//   border-radius: 18px;
+//   background-color: var(--el-fill-color-light);
+// }
 
-.rix > div {
-  background-color: #fff;
-  border: 1px solid #000;
-  border-radius: 4px;
-  padding: 15px; /* 增加内边距 */
-  font-size: 1.2em; /* 增加字体大小 */
-}
-
-.box1 {
-  grid-column: 1 / 3;
-}
-
-.button {
-  display: grid;
-  grid-template-columns: repeat(4, 1fr);
-  grid-template-rows: 175px; /* 增加高度 */
-  grid-gap: 2rem;
-  width: 100%;
-  height: auto;
-}
-
-.button > div {
-  background-color: #fff;
-  border: 1px solid #000;
-  border-radius: 4px;
-  padding: 15px; /* 增加内边距 */
-  font-size: 1.2em; /* 增加字体大小 */
-}
-
-.box2 {
-  grid-column: 1 / 3;
-}
-
-.luik {
+.CmsDisplay2 {
   display: flex;
-  justify-content: space-around;
-  align-items: center;
+  gap: 1rem;
   width: 100%;
+  // padding: 1rem 0.5rem;
+  flex: 1;
+  &-Greater {
+    // padding: 1rem 0.5rem;
+
+    flex: 1;
+
+    border-radius: 18px;
+    background-color: var(--el-fill-color-light);
+  }
+  &-Lesser1 {
+    padding: 1rem 0.5rem;
+
+    flex: 1;
+
+    border-radius: 18px;
+    background-color: var(--el-fill-color-light);
+  }
+  &-Lesser2 {
+    padding: 1rem 0.5rem;
+
+    flex: 1;
+
+    border-radius: 18px;
+    background-color: var(--el-fill-color-light);
+  }
 }
 
-.circle1,
-.circle2,
-.circle3 {
-  border-radius: 100%;
-  width: 60px; /* 增加宽度 */
-  height: 60px; /* 增加高度 */
-  background-color: #000;
-}
+// .circle1,
+// .circle2,
+// .circle3 {
+//   border-radius: 100%;
+//   width: 60px; /* 增加宽度 */
+//   height: 60px; /* 增加高度 */
+//   background-color: #000;
+// }
 </style>
